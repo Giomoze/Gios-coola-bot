485b70fc9beb8b1ec34609e53733e07cff4f9212
Giomoze <96202744+Giomoze@users.noreply.github.com>
2022-11-03 19:11:35 +0100

    added a .env file to hide my tokens

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 module.exports = {
     app: {
-        token: 'OTE0MjU2NTcwMjAyODc4MDAy.GKw84C.CKWpA5y-ptD5pxauNvT661FP2_2cYazLG4OrAw',
+        token: process.env.token,
         playing: 'by Zerio ❤️',
         doing: 'At your service! ❤️',
         gaming: 'Hentai CatMaid',
         global: false,
-        guild: '435140038473547786'
+        guild: process.env.guildID
     },
 
     opt: {
